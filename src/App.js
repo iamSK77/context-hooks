fd92ef5f60fd842f2d17f3117c1cbacb1af600f4
iamSK77 <sk77.mobilefix@gmail.com>
2021-05-28 23:02:06 +0530

    feat: add list comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
+import { Lists } from './components/Lists';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>SK77</h1>
+        <br />
+        <br />
+        <Lists />
       </header>
     </div>
   );
--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export const List = () => {
+export const Lists = () => {
     let id = 0;
-    const [songs, setSongs] = useState([{ title: 'almost home', id: 1 },
-    { title: 'almost home', id: 1 },
-    { title: 'almost home', id: 1 },]);
+    const [songs, setSongs] = useState([
+        { title: "almost home", id: 1 },
+        { title: "almost home", id: 2 },
+        { title: "almost home", id: 3 },
+    ]);
 
     const addSong = () => {
-        setSongs([...songs, { title: 'blinded by the lights', id:  }])
-    }
+        setSongs([...songs, { title: "blinded by the lights", id: id }]);
+    };
 
     return (
         <div>
-
+            <ul>
+                {songs.map((song) => (
+                    <li id={song.id++}>{song.title}</li>
+                ))}
+            </ul>
+            <br />
+            <button onClick={addSong}>AddSong</button>
         </div>
     );
 };
